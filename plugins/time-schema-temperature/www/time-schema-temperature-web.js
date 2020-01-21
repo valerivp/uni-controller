b0299fde78e2ec0215ec5230b063f8c8acfbc29b
ValeriVP <valerivp@gmail.com>
2020-01-22 02:54:22 +0300

    .

--- plugins/time-schema-temperature/www/time-schema-temperature-web.js
+++ plugins/time-schema-temperature/www/time-schema-temperature-web.js
@@ -5,18 +5,6 @@ mm["time-schema"].components.types.add('time-schema-temperature',
         value:{
             type: Number,
             specification: 1,
-/*            fromData: (val)=>(val / 10),
-            toData: (val)=>(val * 10),
-            fromString: (val)=>{
-                if(val !== undefined) {
-                    let v = Number.parseFloat(val);
-                    v = isNaN(v) ? undefined : v;
-                    v = v === undefined ? undefined
-                        : Number(v).toFixed(1);
-                    return v;
-                }
-            },
-            toString: (val)=>Number(val).toFixed(1),
-*/        }
+        }
     }
 );
