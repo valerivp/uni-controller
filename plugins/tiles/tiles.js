61d563be62ef2b7419ef1f2e09fa08b66cad471b
ValeriVP <valerivp@gmail.com>
2020-01-21 13:31:50 +0300

    .

---
+++ plugins/tiles/tiles.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const basedir = require('path').dirname(process.mainModule.filename);
+const db = require(`${basedir}/uc-db`);
+const wscli = require(`${basedir}/uc-wscli`);
+
+module.exports.init = function () {
+    db.init(getDbInitData());
+
+    // noinspection JSUnresolvedVariable
+    let MaxTilesCount = db.querySync("SELECT MaxCount FROM TilesSettings")[0].MaxCount;
+    db.querySync("DELETE FROM TilesParams WHERE ID > $MaxTilesCount", {$MaxTilesCount: MaxTilesCount});
+    for(let i = 1; i <= MaxTilesCount; i++){
+        db.querySync("INSERT OR IGNORE INTO TilesParams (ID) VALUES ($ID)", {$ID: i});
+    }
+};
+
+wscli.context.add('tile');         /** @namespace wscli.context.tile */
+
+wscli.commands.add({Tile: Number}, (arg)=>{
+        wscli.context.current = wscli.context.tile;
+        checkRangeTile(arg, true);
+        wscli.current.tile = arg;
+        return true;
+    },
+    'Set current tile.'
+);
+
+// noinspection JSUnusedLocalSymbols
+function GetInfo(info, arg) {
+    if(wscli.context.current === wscli.context.tile){
+        checkRangeTile(wscli.current.tile, true);
+        let res = false;
+        // noinspection JSUnresolvedVariable
+        let TilesCount = db.querySync("SELECT Count FROM TilesSettings")[0].Count;
+        let q = `SELECT * FROM TilesParams WHERE (ID = $ID OR ($ID = 0 AND ID <= $TilesCount))`;
+        let rows = db.querySync(q, {$ID: wscli.current.tile, $TilesCount: TilesCount});
+        rows.forEach(function (row) { // noinspection JSUnresolvedVariable
+            let data = `#Tile:${row.ID},${info}:${row[info]}`;
+            wscli.sendClientData(data);
+            res = true;
+        });
+        // noinspection JSConstructorReturnsPrimitive
+        if(!res)
+            throw ('No data');
+        return res;
+    }
+}
+
+wscli.commands.add({GetType: null}, GetInfo.bind(undefined, 'Type'), 'Get tile type.');
+wscli.commands.add({GetParams: String}, GetInfo.bind(undefined, 'Params'), 'Get tile params.');
+
+function SetInfo(info, arg) {
+    if(wscli.context.current === wscli.context.tile){
+        checkRangeTile(wscli.current.tile);
+        let qp = {$ID: wscli.current.tile};
+        qp[`\$${info}`] = arg;
+        db.querySync(`UPDATE TilesParams
+            SET ${info} = \$${info}${(info === 'Type' ? ", Params = ''" : "")}
+            WHERE ID = $ID and ${info} != \$${info}`, qp); // проверка на изменение типа чтобы параметры не затереть
+        let row = db.querySync("SELECT * FROM TilesParams WHERE ID = $ID", qp)[0];
+        wscli.sendData(`#Tile:${row.ID},${info}:${row[info]}`);
+        // noinspection JSConstructorReturnsPrimitive
+        return true;
+    }
+
+}
+//wscli.commands.add({SetName: String}, SetInfo.bind(undefined, 'Name'), 'Set tile name.');
+wscli.commands.add({SetType: String}, SetInfo.bind(undefined, 'Type'), 'Set tile type.');
+wscli.commands.add({SetParams: String}, SetInfo.bind(undefined, 'Params'), 'Set tile params.');
+
+// noinspection JSUnusedLocalSymbols
+wscli.commands.add({GetCount: null},(arg) => {
+        if (wscli.context.current === wscli.context.tile) {
+            wscli.checkInRange(wscli.current.tile, 0, 0, 'Tile');
+            let row = db.querySync("SELECT Count FROM TilesSettings")[0];
+            // noinspection JSUnresolvedVariable
+            wscli.sendClientData(`#Tile,Count:${row.Count}`);
+            return true;
+        }
+    },
+    'Get tiles count.'
+);
+
+function checkRangeTile(arg, allowZero) {
+    // noinspection JSUnresolvedVariable
+    return wscli.checkInRange(arg, allowZero ? 0 : 1,
+        db.querySync("SELECT MaxCount FROM TilesSettings")[0].MaxCount, 'Tile');
+}
+
+wscli.commands.add({SetCount: Number}, (arg)=>{
+        if (wscli.context.current === wscli.context.tile) {
+            wscli.checkInRange(wscli.current.tile, 0, 0, 'Tile');
+            checkRangeTile(arg);
+            db.querySync("UPDATE TilesSettings SET Count = $TilesCount", {$TilesCount: arg});
+            let row = db.querySync("SELECT Count FROM TilesSettings")[0];
+            /** @namespace row.Count */
+            wscli.sendData(`#Tile,Count:${row.Count}`);
+            return true;
+        }
+    },
+    'Set tiles count.'
+);
+
+
+// noinspection JSUnusedLocalSymbols
+module.exports.update = function(prevVer){
+    return getDbInitData();
+};
+
+
+function getDbInitData() {
+
+    return `{
+          "main": {
+            "TilesSettings": {
+              "schema": {
+                "ID": "INTEGER PRIMARY KEY AUTOINCREMENT",
+                "MaxCount": "INTEGER NOT NULL",
+                "Count": "INTEGER NOT NULL"
+              },
+              "data": [
+                {"ID": 0, "Count": 1, "MaxCount": 8}
+              ]
+            },
+            "TilesParams": {
+              "schema": {
+                "ID": "INTEGER PRIMARY KEY AUTOINCREMENT",
+                "Type": "CHAR(32) NOT NULL ON CONFLICT REPLACE DEFAULT ''",
+                "Params": "TEXT NOT NULL ON CONFLICT REPLACE DEFAULT ''"
+              }
+            }
+          }
+        }`;
+}
---
+++ plugins/tiles/www/tiles-web.js
@@ -0,0 +1,370 @@
+const TilesComponentsTypes = function () {
+};
+TilesComponentsTypes.prototype.add = function(name, params){
+    this[name] = params;
+    this[name].name = name;
+};
+
+TilesComponentsTypes.prototype.length = function () {
+    let res = 0;
+    // noinspection JSCheckFunctionSignatures
+    Object.keys(this).forEach(() => res++);
+    return res;
+};
+
+const vTilesComponentsTypes = new TilesComponentsTypes;
+module.exports.components = {types: vTilesComponentsTypes};
+
+function Tile(pId) {
+    this.params = {};
+    this._id = pId;
+    this._type = undefined;
+}
+Object.defineProperty(Tile.prototype, 'id', {
+    get() {
+        return this._id;
+    },
+    set(val) {
+        throw('Property is read only');
+    },
+});
+Object.defineProperty(Tile.prototype, 'type', {
+    get() {
+        return this._type;
+    },
+    set(val) {
+        if(val !== this._type){
+            this.params = {};
+            this._type = val;
+        }
+    },
+});
+
+/*
+Tile.prototype = {
+    get id() {
+        return this._id;
+    },
+    set id(val) {
+        throw('Property is read only');
+    },
+    get type() {
+        return this._type;
+    },
+    set type(val) {
+        if(val !== this._type){
+            this.params = {};
+            this._type = val;
+        }
+    },
+};
+*/
+
+function Tiles() {
+}
+
+Tiles.prototype.toArray = function () {
+    let res = [];
+    // noinspection JSCheckFunctionSignatures
+    Object.keys(this).forEach((id)=>res.push(this[id]));
+    return res;
+};
+Tiles.prototype.length = function () {
+    let res = 0;
+    // noinspection JSCheckFunctionSignatures
+    Object.keys(this).forEach(() => res++);
+    return res;
+};
+
+const vTiles = new Vue({
+    data: {
+        tiles: new Tiles(),
+    },
+    computed:{
+        tilesCount: {
+            get(){
+                return this.tiles.length();
+            },
+            set(val){
+                wscli.send(`#Tile,SetCount:${val}`);
+            }
+        },
+    },
+
+    methods: {
+        setTilesCount(val){
+            let tilesCount = this.tiles.length();
+            if(tilesCount !== val) {
+                while (tilesCount < val){
+                    tilesCount++;
+                    Vue.set(this.tiles, tilesCount, new Tile(tilesCount));//{id: tilesCount, params: {}});
+                    wscli.send(`#Tile:${tilesCount},GetType,GetParams`);
+                }
+                while (tilesCount > val)
+                    Vue.delete(this.tiles, tilesCount--);
+            }
+        },
+        changeTileSetting(id){
+            vContent.setTab('tile-settings', {tileId: id});
+        },
+        checkTile(t, allowZero){
+            return checkInRange(t, allowZero ? 0 : 1, this.tiles.length(), "Tile id");
+        },
+
+        getCSSClass(id){
+            let tilesCount = this.tiles.length();
+            let l = [[1], [2,2], [3,3,3], [2,2,2,2], [2,2,3,3,3], [3,3,3,3,3,3], [3,3,3,4,4,4,4],[4,4,4,4,4,4,4,4]];
+            let p = [[1],[1,1],[1,1,1],[1,2,2,1],[2,2,1,2,2],[2,2,2,2,2,2],[2,2,2,2,2,2,1],[2,2,2,2,2,2,2,2]];
+            return `sTilesCount${tilesCount}`
+                + ` l${l[tilesCount - 1][id - 1]}-in-line`
+                + ` p${p[tilesCount - 1][id - 1]}-in-line`
+            ;
+        },
+        setType(id, type){
+            if(this.tiles[id].type !== type){
+                wscli.send(`#Tile:${id},SetType:${type},GetParams`);
+            }
+        },
+        setParams(id, params){
+            let data = `#Tile:${id},SetParams:${wscli.data.toString(params)}`;
+            wscli.send(data);
+        },
+        onShow(params){
+            this.$emit('show');
+            doResizeTilesContent();
+        },
+    },
+    created: function() {
+        module.exports.setParams = this.setParams;
+        ws.on('open', ()=>{
+            wscli.send("#Tile,GetCount");
+        });
+    },
+    template:`
+    <div id="tab-content-tiles" title="Состояние">
+        <div v-for="tile in tiles.toArray()" v-bind:class="getCSSClass(tile.id) + ' sTileWrap'" v-on:click="changeTileSetting(tile.id)">
+            <div class="sTile border3d">
+                <div v-bind:params="tile.params" v-bind:is="tile.type">
+                
+                </div>
+            </div>
+        </div>
+    </div>`
+
+});
+
+vContent.addTab({component: vTiles, id: 'tiles'}, {before: 'sensors', });
+
+
+vTilesComponentsTypes.add('tile-test', {title: 'Компонент'});
+vTilesComponentsTypes.add('tile-second', {title: 'Второй'});
+
+const vTileSettings = new Vue({
+    data: {
+        _isActive: false,
+        _selectedTileId: 1
+    },
+
+    methods: {
+        onShow(params){
+            this.$data._isActive = true;
+            if(params && params.tileId)
+                this.selectedTileId = params.tileId;
+            if(this.selectedTypeName)
+                setTimeout(this.$emit.bind(this, `show-${this.selectedTypeName}-settings`), 2);
+        },
+        onHide(){
+            this.$data._isActive = false;
+            if(this.selectedTypeName)
+                setTimeout(this.$emit.bind(this, `hide-${this.selectedTypeName}-settings`), 1);
+        },
+    },
+    computed:{
+        types(){
+            let res = vTilesComponentsTypes;
+            if(this.selectedTypeName && !res[this.selectedTypeName]){
+                res = Object.assign(new TilesComponentsTypes(), res);
+                res.add(this.selectedTypeName, {title: this.selectedTypeName + ', not installed'});
+            }
+            return res;
+        },
+        //isActive: () => vContent.
+        tiles: ()=> vTiles.tiles,
+        tilesCount: {
+            get(){ return vTiles.tilesCount;},
+            set(val){
+                vTiles.tilesCount = val;
+                if(val && this.selectedTileId > val)
+                    this.selectedTileId = val;
+            }
+        },
+        selectedTile(){
+            return this.tiles[this.selectedTileId] || {}; // для старта, когда нет плиток
+        },
+        selectedTileId: {
+            get(){ return this.$data._selectedTileId; },
+            set(id){
+                this.$data._selectedTileId = id;
+                wscli.send(`#Tile:${id},GetType,GetParams`);
+            }
+        },
+        selectedTypeName: {
+            get: function(){ return this.selectedTile.type; },
+            set: function(t){
+                if(t){
+                    vTiles.setType(this.selectedTileId, t);
+                }
+            }
+        },
+    },
+    watch:{
+        selectedTypeName: function (newVal, oldVal) {
+            if(this.$data._isActive){
+                if(oldVal)
+                    setTimeout(this.$emit.bind(this, `hide-${oldVal}-settings`), 1);
+                if(newVal)
+                    setTimeout(this.$emit.bind(this, `show-${newVal}-settings`), 2);
+            }
+        }
+    },
+    created: function() {
+    },
+    template:`
+    <div id="tab-content-tile-settings" title="Настройка">
+        <div class="sProperties">
+            <div>
+                <div>
+                    <span>Панель</span>
+                    <select v-model="selectedTileId">
+                        <option v-for="tile in tiles" v-bind:value="tile.id">{{tile.id}}</option>
+                    </select>
+                    <div class="button-inc-dec">
+                        <span> из </span>
+                        <button v-on:click="tilesCount--"  class="button-inc-dec">-</button>
+                        <span>{{tilesCount}}</span>
+                        <button v-on:click="tilesCount++"  class="button-inc-dec">+</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <span>Тип данных</span>
+                    <select v-model="selectedTypeName">
+                        <option disabled value="" v-if="!types.length()">не выбрано</option>
+                        <option v-for="type in types" v-bind:value="type.name">{{type.title}}</option>
+                    </select>
+                </div>
+            </div>
+            <div v-bind:is="selectedTypeName + '-settings'" v-if="selectedTypeName"
+                v-bind:type="selectedTypeName + '-settings'"
+                v-bind:tile="selectedTile">
+            
+            </div>
+        </div>
+    </div>`
+
+});
+
+vContent.addTab({component: vTileSettings, id: 'tile-settings'}, {after: 'tiles'});
+
+wscli.context.add('tile');
+wscli.commands.add({Tile: Number}, (arg) => {
+        wscli.context.current = wscli.context.tile;
+        vTiles.checkTile(arg, true);
+        wscli.current.tile = arg;
+        return true;
+    }
+);
+
+function SetInfo(info, arg) {
+     if(wscli.context.current === wscli.context.tile){
+         vTiles.checkTile(wscli.current.tile);
+         Vue.set(vTiles.tiles[wscli.current.tile], info, arg);
+         return true;
+     }
+}
+
+wscli.commands.add({Name: String}, SetInfo.bind(undefined, 'name'));
+wscli.commands.add({Type: String}, SetInfo.bind(undefined, 'type'));
+wscli.commands.add({Params: Object}, SetInfo.bind(undefined, 'params'));
+
+// wscli.commands.add({Type: String}, (arg)=> {
+//         if(wscli.context.current === wscli.context.tile){
+//             vTiles.checkTile(wscli.current.tile);
+//             if(vTiles.tiles[wscli.current.tile].type !== arg)
+//                 Vue.set(vTiles.tiles[wscli.current.tile], "type", arg);
+//             return true;
+//         }
+//     }
+// );
+//
+// wscli.commands.add({Params: Object}, (arg) =>{
+//         if(wscli.context.current === wscli.context.tile){
+//             vTiles.checkTile(wscli.current.tile);
+//             Vue.set(vTiles.tiles[wscli.current.tile], 'params', arg);
+//             return true;
+//         }
+//     }
+// );
+
+wscli.commands.add({Count: Number}, (arg) => {
+        if (wscli.context.current === wscli.context.tile) {
+            checkInRange(wscli.current.tile, 0, 0, 'Tile');
+            vTiles.setTilesCount(arg);
+            doResizeTilesContent();
+            return true;
+        }
+    }
+);
+
+
+// noinspection CssUnusedSymbol
+document.write(`
+<style type="text/css">
+.tile-caption .zoomed-content{
+    zoom: 1;
+    --max-zoom: 2;
+}
+</style>
+`);
+
+
+function doResizeTilesContent() {
+
+    if (doResizeTilesContent.timeoutHandle)
+        clearTimeout(doResizeTilesContent.timeoutHandle);
+    doResizeTilesContent.timeoutHandle = setTimeout(() => {
+        doZoom('.tile-caption .zoomed-content');
+        vTiles.$emit('resize');
+
+    }, 100);
+}
+
+window.addEventListener('resize', doResizeTilesContent, false);
+
+
+Vue.component('tile-test', {
+    props: {
+        tileId: Number
+    },
+    template: '<h3>TEST {{ tileId }}</h3>'
+});
+
+Vue.component('tile-test-settings', {
+    props: {
+        //tileId: Number
+    },
+    template: '<h3>TEST settings {{  }}</h3>'
+});
+
+Vue.component('tile-second', {
+    props: {
+        tileId: Number
+    },
+    template: '<h3>SECOND {{ tileId }}</h3>'
+});
+Vue.component('tile-second-settings', {
+    props: {
+    },
+    template: '<h3>SECOND settings{{  }}</h3>'
+});
